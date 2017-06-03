71dbe7e30dde7ad089ed7e16b0b817cfef9194bd
Robert Jackson <rjackson@linkedin.com>
2017-06-02 22:55:42 -0400

    Only remove in production builds.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ module.exports = {
   name: 'ember-template-module-name-remover',
 
   setupPreprocessorRegistry(type, registry) {
+    if (process.env.EMBER_ENV !== 'production') { return; }
+
 
     registry.add('htmlbars-ast-plugin', {
       name: 'module-name-remover',
